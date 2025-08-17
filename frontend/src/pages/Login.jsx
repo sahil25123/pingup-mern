24178b793651e99317c2a6b2a5281e26876346cb
sahil25123 <sahilgupta25123@gmail.com>
2025-08-18 02:24:23 +0530

    feat: add lucide-react dependency and enhance Login page with background and logo images

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { assets } from '../assets/assets.js'
+
+
 
 function Login() {
   return (
     <div className='min-h-screen flex flex-col md:flex-row'>
-        Login
+      <img src={assets.bgImage} className='absolute top-0 left-0 -z-1 w-full h-full object-cover' ></img>
+        {/* left side  */}
+        <div className='flex-1 flex flex-col items-start justify-between p-6 md:p-10 lg:pl-40'>
+          <img src={assets.logo} className='h-12 object-contain' alt="" />
+          <div>
+            <div>
+              <img src={assets.group_users} className='h-8 md:h-10' alt="" />
+            </div>
+          </div>
+        </div>
       
     </div>
   )
