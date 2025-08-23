41f4163a42c862dcc2f45bf5b99db68320f39a32
sahil25123 <sahilgupta25123@gmail.com>
2025-08-24 00:44:13 +0530

    feat: implement PostCard component for displaying individual posts with user info, content, images, and interaction buttons

--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,41 +1,56 @@
-import React, { useEffect, useState } from 'react'
-import { assets, dummyPostsData } from '../assets/assets';
-import Loading from '../components/Loading';
-import StoryBar from '../components/StoryBar';
+import React, { useEffect, useState } from "react";
+import { assets, dummyPostsData } from "../assets/assets";
+import Loading from "../components/Loading";
+import StoryBar from "../components/StoryBar";
+import PostCard from "../components/PostCard";
 
 function Feed() {
+  const [feeds, setFeeds] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  const [feeds , setFeeds] = useState([]);
-  const [loading, setLoading] = useState(true)
+  const fetchFeeds = async () => {
+    setFeeds(dummyPostsData);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchFeeds();
+  }, []);
 
-  const fetchFeeds =  async()=>{
-    setFeeds(dummyPostsData)
-    setLoading(false)
-  }
-  useEffect(()=>{ 
-    fetchFeeds()
-  } ,[])
-
-  return !loading?   (
-    <div className='h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8'>
+  return !loading ? (
+    <div className="h-full overflow-y-scroll no-scrollbar py-10 xl:pr-5 flex items-start justify-center xl:gap-8">
       {/* {Storied and the post list} */}
-      <StoryBar/>
+      <div className="flex flex-col ">
+        <StoryBar />
 
-
+        <div className="p-4 space-y-6">
+          {feeds.map((post, index) => (
+            <div key={index}>
+              <PostCard key={post._id} post={post} />
+            </div>
+          ))}
+        </div>
+      </div>
 
       {/* Right sidebar */}
-      <div className='max-xl:hidden sticky top-0'>
-        <div className='max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow'>
-          <h3 className='text-slate-800 font-semibold'>Sponsored</h3>
-          <img src={assets.sponsored_img} alt="" className='w-75 h-50 rounded-md' />
-          <p className='text-slate-600'>Email marketing</p>
-          <p className='text-slate-400'>Supercharge your marketing with a powerful, easy-to-use platform built for results.</p>
+      <div className="max-xl:hidden sticky top-0">
+        <div className="max-w-xs bg-white text-xs p-4 rounded-md inline-flex flex-col gap-2 shadow">
+          <h3 className="text-slate-800 font-semibold">Sponsored</h3>
+          <img
+            src={assets.sponsored_img}
+            alt=""
+            className="w-75 h-50 rounded-md"
+          />
+          <p className="text-slate-600">Email marketing</p>
+          <p className="text-slate-400">
+            Supercharge your marketing with a powerful, easy-to-use platform
+            built for results.
+          </p>
         </div>
       </div>
     </div>
-
-
-  ) :<Loading/>
+  ) : (
+    <Loading />
+  );
 }
 
-export default Feed
+export default Feed;
