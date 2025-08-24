a12c229dfc60b2f257b09967ebc8d958128f86bb
sahil25123 <sahilgupta25123@gmail.com>
2025-08-24 17:57:34 +0530

    feat: add Messages page to display user connections with navigation options

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,10 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login";
 import Feed from "./pages/Feed";
+import Messages from "./pages/Messages";
 import Discover from "./pages/Discover";
 import Connections from "./pages/Connections";
 import { useUser } from "@clerk/clerk-react";
-import Dashboard from "./pages/Dashboard";
 import Layout from "./pages/Layout";
 import { Toaster } from "react-hot-toast";
 
@@ -14,17 +14,20 @@ function App() {
 
   return (
     <>
-      <Toaster/>
-        <Routes>
-          <Route path="/" element={<Layout />}></Route>
-          <Route path="/login" element={user ? <Dashboard /> : <Login />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/discover" element={<Discover />} />
-          <Route path="/feed" element={<Feed />} />
-          <Route path="/messages/:userId" element={<Login />} />
-          <Route path="/connections" element={<Connections />} />
-        </Routes>
-     
+      <Toaster />
+      <Routes>
+        <Route path='/' element={!user ? <Login /> : <Layout />}>
+          <Route index element={<Feed />}/>
+          <Route path='messages' element={<Messages />}/>
+          {/* <Route path='messages/:userId' element={<ChatBox />}/> */}
+          <Route path='connections' element={<Connections />}/>
+          <Route path='discover' element={<Discover />}/>
+          {/* <Route path='profile' element={<Profile />}/> */}
+          {/* <Route path='profile/:profileId' element={<Profile />}/> */}
+          {/* <Route path='create-post' element={<CreatePost />}/> */}
+        </Route>
+      
+      </Routes>
     </>
   );
 }
--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -25,7 +25,6 @@ function Layout() {
         : 
         <Menu className='absolute top-3 right-3 p-2 z-100 bg-white rounded-md shadow w-10 h-10 text-gray-600 sm:hidden' onClick={()=>setSidebarOpen(true)}/>
       }
-      <Feed/>
     </div>
 
       
