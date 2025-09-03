53c9772b9b7c5542cdf5ae515d6fcfb18151c248
sahil25123 <sahilgupta25123@gmail.com>
2025-09-04 01:23:48 +0530

    implement updateUserData function and add multer configuration for file uploads

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,6 +1,7 @@
-//Get user  data using userId
-
 import User from "../models/user";
+
+
+//Get user  data using userId
 
 export const getUserData = async (req, res) =>{
     try{
@@ -11,6 +12,42 @@ export const getUserData = async (req, res) =>{
             return res.json({success : false , message :"User not foound"})
         }
         res.json({success:true , user})
+
+    }
+
+    catch(e){
+        console.log(e)
+        res.json({success: false , message : e.message})
+    }
+}
+
+
+export const updateUserData = async (req, res) =>{
+    try{
+        const {userId} = await req.auth();
+
+        const {username , bio , location , full_name } = req.body
+
+
+        const tempUser  = await User.findById(userId);
+
+        if(tempUser.username !== username){
+            const user = User.findOne({username})
+        
+            !username && (username =tempUser.username)
+
+            if(user){
+                //We will not change the username as it is already taken
+                username = tempUser.username
+            }
+        }
+
+        const updatedData ={
+            username , 
+            bio,
+            location, 
+            full_name
+        }
 
     }
 
